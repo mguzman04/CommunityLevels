9d9b04a637ebe3b65c8e761f2f4c37768ba65d41
michael <mgzmn4@gmail.com>
2022-03-13 01:53:18 -0600

    Created basic county layout

--- app/src/main/java/com/mguzma43/communitylevels/County.java
+++ app/src/main/java/com/mguzma43/communitylevels/County.java
@@ -3,8 +3,16 @@ package com.mguzma43.communitylevels;
 public class County {
 
     private String countyName;
+    private String communityLevel;
+    private String bedUtilization;
+    private float cases;
+    private float hospitalAdmission;
 
-    County(String countyName){
+    County(String countyName, String communityLevel, String bedUtilization, float cases, float hospitalAdmission){
         this.countyName = countyName;
+        this.communityLevel = communityLevel;
+        this.bedUtilization = bedUtilization;
+        this.cases = cases;
+        this.hospitalAdmission = hospitalAdmission;
     }
 }
